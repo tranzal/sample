020fc893654c8061bf401f92b0abe357a2929f39
tranzal <64002448+tranzal@users.noreply.github.com>
2022-04-30 21:18:54 +0900

    라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,25 @@
-import {useEffect, useReducer, useState} from 'react'
+import {Component, useEffect, useReducer, useState} from 'react'
 import logo from './logo.svg'
 import './App.css'
 import {getFireStoreData, onMessageListener, requestForToken} from "./util/firebase/firebase";
 import {useDispatch, useSelector} from "react-redux";
 import {RootState} from "./util/redux/store";
+import Sample from "./screen/Sample";
+import {Route, Routes} from "react-router-dom";
 
-function App() {
-  const dispatch = useDispatch()
-  const {token} = useSelector((state : RootState) => state.token)
+const App = () => {
 
-  const requestToken = async () =>{
-    const currentToken : string = await requestForToken() as string
-    dispatch({type:"changeToken", token : currentToken})
-  }
+    const routes = [
+        {path: "/Test",component : <Sample/>},
+        {path: "/sample",component : <Sample/>},
+    ]
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{token}</p>
-        <p>
-          <button type="button" onClick={() => requestToken()}>
-            토큰 호출
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    return(
+        <Routes>
+            {routes.map((route : {component : JSX.Element, path : string}, index : number) => (
+                <Route path={route.path} element={route.component} />
+            ))}
+        </Routes>
   )
 }
 
